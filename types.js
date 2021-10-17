844e772af1ebc1857d90b77584e50c0d56648ebc
Enberg65 <emilnberg@gmail.com>
2021-10-17 10:45:07 +0200

    fixing things, trying to stop rebase

---
+++ types.js
@@ -0,0 +1,25 @@
+// const middle = 'topher';
+// const last = `bos`;
+
+// const sentence = 'shes\'s so "cool"';
+// const sentence2 = `she\`s's so "cool"`;
+
+// const song = `Ohhh
+// ya
+// I like
+// pizza`;
+
+// const name = 'Wes';
+// const html = `hello my name is ${name}. Nice to meet you`;
+// const cher = {
+//   first: 'Cher',
+// };
+// const teller = {
+//   first: 'Raymond',
+//   last: 'Teller',
+// };
+// teller.first = 'Teller';
+// teller.last = null;
+// document.body.innerHTML = html;
+
+const isDrawing = false;
